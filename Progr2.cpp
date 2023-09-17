387c5fb9a1b332e289b01fbfe2da9d0ed9e1394c
Daniletskiy-D <slimedodoser@gmail.com>
2023-09-17 19:09:46 +0300

    2

--- Progr2.cpp
+++ Progr2.cpp
@@ -3,4 +3,20 @@
 int main() {
     char op;
     double a, b, res;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+
+    std::cout << "Enter operation: ";
+    std::cin >> op;
+
+    std::cout << "Enter second operation: ";
+    std::cin >> b;
+
+    switch (operation) {
+    case '+':
+        result = operand1 + operand2;
+        break;
+    }
+
 }
